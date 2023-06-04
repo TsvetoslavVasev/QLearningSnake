3252a5467d8fd0ecc6b9d913d003eddea2fb5164
Tsvetoslav Vasev <cvasev@Alfred.local>
2023-06-04 16:10:31 +0300

    feat: use mps device for training

--- agent.py
+++ agent.py
@@ -1,38 +1,36 @@
-import torch 
-import random 
+import torch
+import random
 import numpy as np
 from collections import deque
-from snake_gameai import SnakeGameAI,Direction,Point,BLOCK_SIZE
-from model import Linear_QNet,QTrainer
+from snake_gameai import SnakeGameAI, Direction, Point, BLOCK_SIZE
+from model import Linear_QNet, QTrainer
 from Helper import plot
+
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
+# Check MPS availability and enable it
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+else:
+    mps_device = torch.device("mps")  # Set the MPS device
+
 class Agent:
     def __init__(self):
         self.n_game = 0
-        self.epsilon = 0 # Randomness
-        self.gamma = 0.9 # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = Linear_QNet(11,256,3) 
-        self.trainer = QTrainer(self.model,lr=LR,gamma=self.gamma)
-        # for n,p in self.model.named_parameters():
-        #     print(p.device,'',n) 
-        # self.model.to('cuda')   
-        # for n,p in self.model.named_parameters():
-        #     print(p.device,'',n)         
-        # TODO: model,trainer
+        self.epsilon = 0  # Randomness
+        self.gamma = 0.9  # Discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
+        self.model = Linear_QNet(11, 256, 3).to(mps_device)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
-    # state (11 Values)
-    #[ danger straight, danger right, danger left,
-    #   
-    # direction left, direction right,
-    # direction up, direction down
-    # 
-    # food left,food right,
-    # food up, food down]
-    def get_state(self,game):
+    def get_state(self, game):
         head = game.snake[0]
         point_l=Point(head.x - BLOCK_SIZE, head.y)
         point_r=Point(head.x + BLOCK_SIZE, head.y)
@@ -45,66 +43,60 @@ class Agent:
         dir_d = game.direction == Direction.DOWN
 
         state = [
-            # Danger Straight
-            (dir_u and game.is_collision(point_u))or
-            (dir_d and game.is_collision(point_d))or
-            (dir_l and game.is_collision(point_l))or
-            (dir_r and game.is_collision(point_r)),
-
-            # Danger right
-            (dir_u and game.is_collision(point_r))or
-            (dir_d and game.is_collision(point_l))or
-            (dir_u and game.is_collision(point_u))or
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
             (dir_d and game.is_collision(point_d)),
 
-            #Danger Left
-            (dir_u and game.is_collision(point_r))or
-            (dir_d and game.is_collision(point_l))or
-            (dir_r and game.is_collision(point_u))or
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
             (dir_l and game.is_collision(point_d)),
 
-            # Move Direction
             dir_l,
             dir_r,
             dir_u,
             dir_d,
 
-            #Food Location
-            game.food.x < game.head.x, # food is in left
-            game.food.x > game.head.x, # food is in right
-            game.food.y < game.head.y, # food is up
-            game.food.y > game.head.y  # food is down
+            game.food.x < game.head.x,
+            game.food.x > game.head.x,
+            game.food.y < game.head.y,
+            game.food.y > game.head.y
         ]
-        return np.array(state,dtype=int)
+        return np.array(state, dtype=int)
 
-    def remember(self,state,action,reward,next_state,done):
-        self.memory.append((state,action,reward,next_state,done)) # popleft if memory exceed
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        if (len(self.memory) > BATCH_SIZE):
-            mini_sample = random.sample(self.memory,BATCH_SIZE)
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
         else:
             mini_sample = self.memory
-        states,actions,rewards,next_states,dones = zip(*mini_sample)
-        self.trainer.train_step(states,actions,rewards,next_states,dones)
 
-    def train_short_memory(self,state,action,reward,next_state,done):
-        self.trainer.train_step(state,action,reward,next_state,done)
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_game
-        final_move = [0, 0, 0]
+        final_move = [0,0,0]
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
         else:
-            state0 = torch.tensor(state, dtype=torch.float)  # Removed .cuda() from here
-            prediction = self.model(state0)  # Removed .cuda() from here
+            state0 = torch.from_numpy(np.array(state, dtype=np.float32)).to(mps_device)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         return final_move
-
 
 def train():
     plot_scores = []
@@ -114,38 +106,48 @@ def train():
     agent = Agent()
     game = SnakeGameAI()
     while True:
-        # Get Old state
         state_old = agent.get_state(game)
-
-        # get move
         final_move = agent.get_action(state_old)
-
-        # perform move and get new state
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
 
-        # train short memory
-        agent.train_short_memory(state_old,final_move,reward,state_new,done)
-
-        #remember
-        agent.remember(state_old,final_move,reward,state_new,done)
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
-            # Train long memory,plot result
             game.reset()
             agent.n_game += 1
             agent.train_long_memory()
-            if(score > reward): # new High score 
-                reward = score
+
+            if score > record:
+                record = score
                 agent.model.save()
-            print('Game:',agent.n_game,'Score:',score,'Record:',record)
-            
+
+            print('Game', agent.n_game, 'Score', score, 'Record:', record)
+
             plot_scores.append(score)
-            total_score+=score
+            total_score += score
             mean_score = total_score / agent.n_game
             plot_mean_scores.append(mean_score)
-            plot(plot_scores,plot_mean_scores)
+            plot(plot_scores, plot_mean_scores)
+
+def play_game_with_pretrained_model():
+    agent = Agent()
+    game = SnakeGameAI()
+
+    # Load the pretrained model
+    agent.model.load('850-epoch.pth')
+
+    while True:
+        state_old = agent.get_state(game)
+        final_move = agent.get_action(state_old, use_epsilon=False)
+        _, done, score = game.play_step(final_move)
+
+        if done:
+            game.reset()
+            print('Game Over. Score', score)
 
 
-if(__name__=="__main__"):
-    train()
+if __name__ == '__main__':
+    # train()
+    play_game_with_pretrained_model()
--- model.py
+++ model.py
@@ -3,12 +3,32 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+import random
+import numpy as np
+from collections import deque
+from snake_gameai import SnakeGameAI, Direction, Point, BLOCK_SIZE
+from Helper import plot
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+# Check MPS availability and enable it
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+else:
+    mps_device = torch.device("mps")  # Set the MPS device
 
 class Linear_QNet(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
         super().__init__()
-        self.linear1 = nn.Linear(input_size,hidden_size)  # Removed .cuda() from here
-        self.linear2 = nn.Linear(hidden_size,output_size)  # Removed .cuda() from here
+        self.linear1 = nn.Linear(input_size,hidden_size).to(mps_device)
+        self.linear2 = nn.Linear(hidden_size,output_size).to(mps_device)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
@@ -18,35 +38,40 @@ class Linear_QNet(nn.Module):
     def save(self, file_name='model.pth'):
         model_folder_path = '/Users/cvasev/Developer/QLearningSnake'
         file_name = os.path.join(model_folder_path,file_name)
-        torch.save(self.state_dict(),file_name)
+        torch.save(self.state_dict(), file_name)
+    
+    def load(self, file_name='850-epoch.pth'):
+        model_folder_path = '/Users/cvasev/Developer/QLearningSnake'
+        file_name = os.path.join(model_folder_path, file_name)
+        self.load_state_dict(torch.load(file_name))
 
 class QTrainer:
     def __init__(self,model,lr,gamma):
         self.lr = lr
         self.gamma = gamma
-        self.model = model
+        self.model = model.to(mps_device)
         self.optimer = optim.Adam(model.parameters(),lr = self.lr)    
         self.criterion = nn.MSELoss()
 
     def train_step(self,state,action,reward,next_state,done):
-        state = torch.tensor(state,dtype=torch.float)  # Removed .cuda() from here
-        next_state = torch.tensor(next_state,dtype=torch.float)  # Removed .cuda() from here
-        action = torch.tensor(action,dtype=torch.long)  # Removed .cuda() from here
-        reward = torch.tensor(reward,dtype=torch.float)  # Removed .cuda() from here
+        state = torch.from_numpy(np.array(state, dtype=np.float32)).to(mps_device)
+        next_state = torch.from_numpy(np.array(next_state, dtype=np.float32)).to(mps_device)
+        action = torch.from_numpy(np.array(action, dtype=np.int64)).to(mps_device)  # Use np.int64 here
+        reward = torch.from_numpy(np.array(reward, dtype=np.float32)).to(mps_device)
 
         if(len(state.shape) == 1): 
-            state = torch.unsqueeze(state,0)  # Removed .cuda() from here
-            next_state = torch.unsqueeze(next_state,0)  # Removed .cuda() from here
-            action = torch.unsqueeze(action,0)  # Removed .cuda() from here
-            reward = torch.unsqueeze(reward,0)  # Removed .cuda() from here
+            state = torch.unsqueeze(state,0).to(mps_device)
+            next_state = torch.unsqueeze(next_state,0).to(mps_device)
+            action = torch.unsqueeze(action,0).to(mps_device)
+            reward = torch.unsqueeze(reward,0).to(mps_device)
             done = (done, )
 
-        pred = self.model(state)  # Removed .cuda() from here
-        target = pred.clone()  # Removed .cuda() from here
+        pred = self.model(state)
+        target = pred.clone()
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))  # Removed .cuda() from here
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
             target[idx][torch.argmax(action).item()] = Q_new 
 
         self.optimer.zero_grad()
